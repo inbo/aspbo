20ebd6278f1c8f78b95c04c96aa628fb219ccdc4
Sander Devisscher <sander.devisscher@inbo.be>
2023-08-25 17:34:30 +0200

    fix it better ?
    #6

--- src/move_aws_login.R
+++ src/move_aws_login.R
@@ -1,4 +1,4 @@
 system('powershellNew-Item -ItemType directory -Path $HOME\bin')
 system('Copy-Item "~src\aws-cli-mfa-login.py" 
        -Destination "$HOME\bin\aws-cli-mfa-login"')
-system('Copy-Item "~src\common.py" -Destination "$HOME\bin"')
+system('Copy-Item "~src/common.py" -Destination "$HOME\bin"')
