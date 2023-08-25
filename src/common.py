4210e257e3265757e2075af42fdc0f6a3c7ff171
Sander Devisscher <sander.devisscher@inbo.be>
2023-08-25 17:24:37 +0200

    Create common.py
    #6

---
+++ src/common.py
@@ -0,0 +1,7 @@
+AWS_ACCOUNTS = {
+    'inbo-networking': '255728642082',
+    'inbo-shared-infra': '347082780157',
+    'inbo-prod': '800040084629',
+    'inbo-dev': '632683202044',
+    'inbo-uat': '625469168093',
+}
