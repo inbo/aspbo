b1ae0cf6a6370a44ede726be42407a2a3f4c3556
Sander Devisscher <sander.devisscher@inbo.be>
2023-08-25 11:47:49 +0200

    Update connect_to_bucket.R
    @TheJenne18 ik heb een functie geschreven waarmee we de sessiontoken kunnen verversen. Kan je eens nalezen dat ik het correct beschrijf ?
    
    #6

--- src/connect_to_bucket.R
+++ src/connect_to_bucket.R
@@ -1,14 +1,48 @@
-connect_to_bucket <- function(bucket_type = "inbo-uat", 
-                              bucket_name,
-                              role = "inbo-developers-fis-role", 
-                              boto3 = FALSE){
+#' @author Sander Devisscher
+#' @author Jens Polspoel
+#' 
+#' @description
+#' Deze functie refresht de sessiontoken nodig om contact te leggen met inbo
+#' AWS S3 buckets.
+#' 
+#' @param bucket_name String met de naam van de bucket.
+#' @param bucket_type String met het type bucket. ofwel "inbo-uat" of "inbo-prod"
+#' @param role String met de rechtengroep waartoe je behoort. Default is:
+#' "inbo-developers-fis-role"
+#' 
+#' @details
+#' # Voorbereiding:
+#' De volgende programma's moeten worden geïnstalleerd vooraleer je deze functie 
+#' kunt gebruiken:
+#' -*AWS CLI* moet worden geïnstalleerd als dat nog niet gebeurd is. Hiervoor
+#' heb je admin rechten nodig, een _ict helpdesk call_ is dus aan de orde.
+#' Na de installatie moeten je *AWS credentials* eenmalig aangemaakt worden. 
+#' Voer hiervoor `aws configure` uit in _windows powershell_. De credentials
+#' kan je bekomen bij Jens Polspoel.
+#' -*devops-toolkit* moet lokaal worden geïnstalleerd dit doe je door 
+#' *https://github.com/inbo/devops-toolkit* te clonen dmv _github dekstop_. 
+#' Vervolgens kopiëer je *aws-cli-mfa-login.py* & *common.py* naar de Home 
+#' Directory van Windows. Default is dat *C:\Users\<voornaam_achternaam>\bin*.
+#' -*Python* moet worden geïnstalleerd als dat nog niet gebeurd is. Dit kan 
+#' rechtstreeks in _R_ met onderstaande code:
+#' `install.packages("reticulate")`
+#' `reticulate::install_python()`
+#' `system('pip install boto3')`
+#' 
+#' @return Dataframe met een lijst van bestanden op de bucket met relevante info.
+#' 
+#' @examples
+#' # Alleen de bucket_name is uiterst noodzakelijk:
+#' # source("./src/connect_to_bucket.R")
+#' # connect_to_bucket("inbo-exotenportaal-uat-eu-west-1-default")
+#' 
+#' 
+
+connect_to_bucket <- function(bucket_name,
+                              bucket_type = "inbo-uat", 
+                              role = "inbo-developers-fis-role"){
   # Libraries ####
   library(aws.s3)
-  
-  # Install boto3 ####
-  if(boto3 == TRUE){
-    system('pip install boto3')
-  }
   
   # Refresh session token ####
   # Onderstaande code maakt een profiel aan met een sessiontoken die één uur 
