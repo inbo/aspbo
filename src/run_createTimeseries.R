3f7426411c7cfd4349bcd17e31eb850303ab3832
Sander Devisscher <sander.devisscher@inbo.be>
2023-09-08 18:06:28 +0200

    Update run_createTimeseries.R
    #10

--- src/run_createTimeseries.R
+++ src/run_createTimeseries.R
@@ -1,15 +1,3 @@
-# update alienSpecies 
-inbotheme_version <- as.data.frame(installed.packages()) %>% 
-  filter(Package == "INBOtheme")
-
-if(inbotheme_version$Version != "0.5.8"){
-  remove.packages("INBOtheme")
-  devtools::install_github("inbo/INBOtheme@v0.5.8")
-}
-
-devtools::install_github("inbo/alien-species-portal@sprint_v0.0.4", 
-                         subdir = "alienSpecies", force = TRUE)
-
 # Create full_timeseries.csv
 alienSpecies::createTimeseries(dataDir = "./data/interim/",
                                shapeData = alienSpecies::readShapeData()$utm1_bel_with_regions,
