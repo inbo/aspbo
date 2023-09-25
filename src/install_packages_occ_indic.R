0b76d4c2cee016c1e6564d9f07b685b6c160787d
Sander Devisscher <sander.devisscher@inbo.be>
2023-09-25 10:32:17 +0200

    Update install_packages_occ_indic.R
    #10

--- src/install_packages_occ_indic.R
+++ src/install_packages_occ_indic.R
@@ -1,8 +1,10 @@
 # load required packages (install them if needed)
 installed <- rownames(installed.packages())
-required <- c("devtools", "knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
+required <- c("knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
               "progress", "here", "lubridate", "readr", "purrr",
               "stringr", "tidyr")
 if (!all(required %in% installed)) {
   install.packages(required[!required %in% installed])
 }
+
+install.packages("devtools", dependencies = TRUE)
