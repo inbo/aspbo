d47e2c307e90b1e5703f6f3e52d8c35f07c52e33
Sander Devisscher <sander.devisscher@inbo.be>
2023-09-25 15:29:04 +0200

    Update install_packages_occ_indic.R
    #10

--- src/install_packages_occ_indic.R
+++ src/install_packages_occ_indic.R
@@ -1,10 +1,10 @@
 # load required packages (install them if needed)
 installed <- rownames(installed.packages())
-required <- c("knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
+required <- c("devtools", "knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
               "progress", "here", "lubridate", "readr", "purrr",
-              "stringr", "tidyr", "pkgdown")
+              "stringr", "tidyr")
 if (!all(required %in% installed)) {
   install.packages(required[!required %in% installed])
 }
 
-install.packages("devtools", dependencies = TRUE)
+install_github("inbo/INBOtheme@v0.5.8")
