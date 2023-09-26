4229192290b74a3180d2438f725f031e78584afc
Sander Devisscher <sander.devisscher@inbo.be>
2023-09-26 10:12:07 +0200

    try inbotheme fix
    #10

--- src/install_alienSpecies.R
+++ src/install_alienSpecies.R
@@ -3,14 +3,5 @@ library(magrittr)
 library(dplyr)
 library(devtools)
 
-# update alienSpecies 
-inbotheme_version <- as.data.frame(installed.packages()) %>% 
-  filter(Package == "INBOtheme")
-
-if(inbotheme_version$Version != "0.5.8"){
-  remove.packages("INBOtheme")
-  install_github("inbo/INBOtheme@v0.5.8")
-}
-
 install_github("inbo/alien-species-portal@sprint_v0.0.4", 
                          subdir = "alienSpecies", force = TRUE)
