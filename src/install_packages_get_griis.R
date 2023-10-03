5580325a18312d3d0e3b4231ceb405921c96cf5f
Sander Devisscher <sander.devisscher@inbo.be>
2023-10-03 11:22:32 +0200

    Create install_packages_get_griis.R
    #2

---
+++ src/install_packages_get_griis.R
@@ -0,0 +1,12 @@
+# get packages installed on machine
+installed <- rownames(installed.packages())
+# specify packages we need
+required <- c("rgbif", "magrittr", "readr",
+              "dplyr"
+)
+# install packages if needed
+if (!all(required %in% installed)) {
+  pkgs_to_install <- required[!required %in% installed]
+  print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
+  install.packages(pkgs_to_install)
+}
