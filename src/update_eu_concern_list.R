7d6d1a9df0e06817664aeedcbb7b585682e3e189
Sander Devisscher <sander.devisscher@inbo.be>
2023-10-20 17:16:57 +0200

    Create update_eu_concern_list.R
    #26

---
+++ src/update_eu_concern_list.R
@@ -0,0 +1,62 @@
+# Libraries ####
+library(readr)
+library(tidyr)
+library(magrittr)
+library(dplyr)
+library(aws.s3)
+
+# connect to bucket ####
+source("./src/connect_to_bucket.R")
+
+UAT_filelist <- connect_to_bucket(bucket_name = Sys.getenv("UAT_bucket"))
+
+# get files ####
+eu_concern_list_old <- read_delim(rawToChar(get_object(bucket = Sys.getenv("UAT_bucket"),
+                                  region = "eu-west-1",
+                                  object = "eu_concern_species.tsv",
+                                  as = "raw")), delim = "\t") 
+
+eu_concern_list_new <- read_delim("https://raw.githubusercontent.com/trias-project/indicators/main/data/input/eu_concern_species.tsv", 
+                                  delim = "\t", escape_double = FALSE, 
+                                  col_types = cols(entry_into_force = col_date(format = "%Y-%m-%d")), 
+                                  trim_ws = TRUE) 
+
+# fix known issues ####
+## Vespa velutina nigrithorax: subspp -> spp 
+eu_concern_list_new <- eu_concern_list_new %>% 
+  mutate(backbone_taxonKey = case_when(checklist_scientificName == "Vespa velutina nigrithorax" 
+                                       & backbone_taxonKey == 6247411 ~ 1311477,
+                                       TRUE ~ backbone_taxonKey))
+
+if(nrow(eu_concern_list_new) > nrow(eu_concern_list_old)){
+  ## list has expanded ####
+  # write new list to output to trigger upload
+  write_tsv(eu_concern_list_new, "./data/output/eu_concern_species.tsv")
+}else{
+  ## list has not expanded ####
+  new_taxonKeys <- subset(eu_concern_list_new$backbone_taxonKey,
+                          !eu_concern_list_new$backbone_taxonKey %in% 
+                            eu_concern_list_old$backbone_taxonKey)
+  
+  lists_combined <- eu_concern_list_new %>% 
+    full_join(eu_concern_list_old, by= "checklist_scientificName")
+  
+  changed_taxonKeys <- lists_combined %>% 
+    filter(!is.na(backbone_taxonKey.x),
+           !is.na(backbone_taxonKey.y)) %>% 
+    filter(backbone_taxonKey.x != backbone_taxonKey.y)
+  
+  omited_taxonKeys <- lists_combined %>% 
+    filter(is.na(backbone_taxonKey.x))
+  
+  if(length(new_taxonKeys) > 0 | 
+     nrow(changed_taxonKeys) > 0 |
+     nrow(omited_taxonKeys) > 0){
+    ### list has changed ####
+    # write new list to output to trigger upload
+    write_tsv(eu_concern_list_new, "./data/output/eu_concern_species.tsv")
+  }else{
+    ### No changes ####
+    warning("no changes detected! eu concern list not updated!")
+  }
+}
