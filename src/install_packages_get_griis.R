66636c2ea0d9fac06b6a072b9df1e7ddfab928e0
Sander Devisscher <sander.devisscher@inbo.be>
2023-12-20 12:37:16 +0100

    add install trias
    #57

--- src/install_packages_get_griis.R
+++ src/install_packages_get_griis.R
@@ -1,7 +1,7 @@
 # get packages installed on machine
 installed <- rownames(installed.packages())
 # specify packages we need
-required <- c("tidyverse", "rgbif", "tidylog", "janitor", "here", "trias"
+required <- c("tidyverse", "rgbif", "tidylog", "janitor", "here", "devtools"
 )
 # install packages if needed
 if (!all(required %in% installed)) {
@@ -9,3 +9,6 @@ if (!all(required %in% installed)) {
   print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
   install.packages(pkgs_to_install, repos = "https://cran.r-project.org/")
 }
+
+# install trias
+devtools::install_github("trias-project/trias")
