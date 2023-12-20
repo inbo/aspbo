88e09f249cf951d3da0d2a093fd695b5c7ab7302
Sander Devisscher <sander.devisscher@inbo.be>
2023-12-20 10:48:50 +0100

    try again
    #57

--- src/install_packages_get_griis.R
+++ src/install_packages_get_griis.R
@@ -7,5 +7,5 @@ required <- c("rgbif", "tidyverse", "tidylog", "janitor", "here", "trias"
 if (!all(required %in% installed)) {
   pkgs_to_install <- required[!required %in% installed]
   print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
-  install.packages(pkgs_to_install)
+  install.packages(pkgs_to_install, repos = "https://cran.r-project.org/")
 }
