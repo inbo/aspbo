8346f1dd131df5406615726b37282f1c349dd043
Sander Devisscher <sander.devisscher@inbo.be>
2024-03-28 17:44:12 +0100

    set packages
    #8

---
+++ src/Vespa velutina management/install_packages_vespa_velutina_gbif.R
@@ -0,0 +1,10 @@
+# get packages installed on machine
+installed <- rownames(installed.packages())
+# specify packages we need
+required <- c("rgbif", "dplyr", "sf", "readr", "magrittr")
+# install packages if needed
+if (!all(required %in% installed)) {
+  pkgs_to_install <- required[!required %in% installed]
+  print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
+  install.packages(pkgs_to_install)
+}
