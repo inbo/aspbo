0b06f2d3537365902a4ca9444fb9db5a1eed1933
Sander Devisscher <sander.devisscher@inbo.be>
2024-04-25 12:38:39 +0200

    Create install_packages_oxyura_jamaicensis_management.R
    #53

---
+++ src/Oxyura jamaicensis management/install_packages_oxyura_jamaicensis_management.R
@@ -0,0 +1,18 @@
+# get packages installed on machine
+installed <- rownames(installed.packages())
+# specify packages we need
+required <- c(
+  "rgbif",
+  "dplyr",
+  "readr", 
+  "magrittr",
+  "tidyr",
+  "stringr",
+  "testthat"
+)
+# install packages if needed
+if (!all(required %in% installed)) {
+  pkgs_to_install <- required[!required %in% installed]
+  print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
+  install.packages(pkgs_to_install)
+}
