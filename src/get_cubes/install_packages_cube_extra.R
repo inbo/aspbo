dcc003fd67f2e1a9e8dc7da5c2582b32131c7497
Sander Devisscher <sander.devisscher@inbo.be>
2024-10-23 17:14:40 +0200

    Create install_packages_cube_extra.R
    #18

---
+++ src/get_cubes/install_packages_cube_extra.R
@@ -0,0 +1,10 @@
+# get packages installed on machine
+installed <- rownames(installed.packages())
+# specify packages we need
+required <- c("rgbif")
+# install packages if needed
+if (!all(required %in% installed)) {
+  pkgs_to_install <- required[!required %in% installed]
+  print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
+  install.packages(pkgs_to_install, repos = "https://cran.r-project.org/")
+}
