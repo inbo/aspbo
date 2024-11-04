0bed49d3134c69ef3498abbcfe1c84ffaaca55af
Sander Devisscher <sander.devisscher@inbo.be>
2024-11-04 16:45:18 +0100

    add knitr
    #18

--- src/get_cubes/install_packages_cube_base.R
+++ src/get_cubes/install_packages_cube_base.R
@@ -1,7 +1,7 @@
 # get packages installed on machine
 installed <- rownames(installed.packages())
 # specify packages we need
-required <- c("magrittr", "dplyr", "readr")
+required <- c("magrittr", "dplyr", "readr", "knitr")
 # install packages if needed
 if (!all(required %in% installed)) {
   pkgs_to_install <- required[!required %in% installed]
