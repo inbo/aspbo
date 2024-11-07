fb4e99f0b2e874cc55e904aa936eb247c97cd5df
Sander Devisscher <sander.devisscher@inbo.be>
2024-11-07 15:56:56 +0100

    add stringr
    #18

--- src/get_cubes/install_packages_cube_base.R
+++ src/get_cubes/install_packages_cube_base.R
@@ -1,7 +1,7 @@
 # get packages installed on machine
 installed <- rownames(installed.packages())
 # specify packages we need
-required <- c("magrittr", "dplyr", "readr", "knitr")
+required <- c("magrittr", "dplyr", "readr", "knitr", "stringr")
 # install packages if needed
 if (!all(required %in% installed)) {
   pkgs_to_install <- required[!required %in% installed]
