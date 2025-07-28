9a9f0185427de235786055676a4fc7e7344a36ad
Sander Devisscher <sander.devisscher@inbo.be>
2025-07-28 14:37:00 +0200

    fix #484

--- src/install_packages_upload_files.R
+++ src/install_packages_upload_files.R
@@ -1,8 +1,7 @@
 # get packages installed on machine
 installed <- rownames(installed.packages())
 # specify packages we need
-required <- c(
-  "aws.s3", "magrittr", "readr",
+required <- c("aws.s3", "magrittr", "readr",
   "dplyr", "tidyr", "stringr",
   "testthat"
 )
@@ -15,7 +14,10 @@ if (!all(required %in% installed)) {
 
 # Test if minimum version of aws.s3 is installed
 if (packageVersion("aws.s3") < "0.3.22") {
-  stop("Please update aws.s3 to version 0.3.21 or higher.")
+  install.packages("devtools")
+  cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n
+      ==> installing v0.3.22 from https://rforge.net")
+  devtools::install_version("aws.s3", version = "0.3.22", repos = "https://rforge.net")
 }else{
   cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n")
 }
