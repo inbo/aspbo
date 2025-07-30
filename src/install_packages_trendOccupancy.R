ae3cf2ef2a87eb97c581593ce4ad3ef14b7470bd
Sander Devisscher <sander.devisscher@inbo.be>
2025-07-30 17:21:25 +0200

    add sf
    #447
    #304

---
+++ src/install_packages_trendOccupancy.R
@@ -0,0 +1,16 @@
+# load required packages (install them if needed)
+installed <- rownames(installed.packages())
+required <- c("devtools", "knitr", "dplyr", "magrittr", "readr", 
+              "testthat", "sf")
+if (!all(required %in% installed)) {
+  install.packages(required[!required %in% installed])
+}
+
+# Test if minimum version of aws.s3 is installed
+if (packageVersion("aws.s3") < "0.3.22") {
+  cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n
+      ==> installing v0.3.22 from https://rforge.net")
+  devtools::install_version("aws.s3", version = "0.3.22", repos = "https://rforge.net")
+}else{
+  cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n")
+}
