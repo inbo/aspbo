88cbf721bba28ab5af550226ededdab81a806881
Sander Devisscher <sander.devisscher@inbo.be>
2026-03-16 11:00:45 +0100

    use remotes instead of devtools
    #608
    
    related to #612

--- src/process_cubes/install_packages_occ_indic.R
+++ src/process_cubes/install_packages_occ_indic.R
@@ -1,7 +1,7 @@
 # install CRAN packages ####
 # load required packages (install them if needed)
 installed <- rownames(installed.packages())
-required <- c("devtools", "knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
+required <- c("remotes", "knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
               "progress", "here", "lubridate", "readr", "purrr",
               "stringr", "tidyr", "aws.s3", "sf", "testthat", "pbapply")
 if (!all(required %in% installed)) {
@@ -13,13 +13,13 @@ if (packageVersion("aws.s3") < "0.3.22") {
   install.packages("devtools")
   cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n
       ==> installing v0.3.22 from https://rforge.net")
-  devtools::install_version("aws.s3", version = "0.3.22", repos = "https://rforge.net")
+  remotes::install_version("aws.s3", version = "0.3.22", repos = "https://rforge.net")
 }else{
   cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n")
 }
 
 # install non-CRAN packages ####
-devtools::install_github("trafficonese/leaflet.extras", force = TRUE)
-devtools::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
-devtools::install_github("inbo/alien-species-portal@uat", 
+remotes::install_github("trafficonese/leaflet.extras", force = TRUE)
+remotes::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
+remotes::install_github("inbo/alien-species-portal@uat", 
                          subdir = "alienSpecies", force = TRUE)
