891cac4715718f0967bde25af0ae7043a96e1bc2
Sander Devisscher <sander.devisscher@inbo.be>
2026-04-07 16:25:18 +0200

    Update install_packages_eu_concern_list.R
    #620

--- src/install_packages_eu_concern_list.R
+++ src/install_packages_eu_concern_list.R
@@ -3,7 +3,7 @@
 installed <- rownames(installed.packages())
 # specify packages we need
 required <- c("aws.s3", "magrittr", "readr",
-              "dplyr", "rgbif", "devtools"
+              "dplyr", "rgbif", "remotes"
 )
 # install packages if needed
 if (!all(required %in% installed)) {
@@ -13,7 +13,7 @@ if (!all(required %in% installed)) {
 }
 
 # install non-CRAN packages ####
-devtools::install_github("trafficonese/leaflet.extras", force = TRUE)
-devtools::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
-devtools::install_github("inbo/alien-species-portal@uat", 
+remotes::install_github("trafficonese/leaflet.extras", force = TRUE)
+remotes::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
+remotes::install_github("inbo/alien-species-portal@uat", 
                          subdir = "alienSpecies", force = TRUE)
