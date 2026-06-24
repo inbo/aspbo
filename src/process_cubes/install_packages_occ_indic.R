f88e2f5ee310bd7683eeee3cf28e071099a5a77f
Sander Devisscher <sander.devisscher@inbo.be>
2026-06-24 16:32:50 +0200

    improve package installation
    #623

--- src/process_cubes/install_packages_occ_indic.R
+++ src/process_cubes/install_packages_occ_indic.R
@@ -4,8 +4,9 @@ installed <- rownames(installed.packages())
 required <- c("remotes", "knitr", "dplyr", "magrittr", "rgbif", "tidylog", 
               "progress", "here", "lubridate", "readr", "purrr",
               "stringr", "tidyr", "aws.s3", "sf", "testthat", "pbapply")
+
 if (!all(required %in% installed)) {
-  install.packages(required[!required %in% installed])
+  install.packages(required[!required %in% installed], dependencies = TRUE)
 }
 
 # Test if minimum version of aws.s3 is installed
@@ -18,8 +19,13 @@ if (packageVersion("aws.s3") < "0.3.22") {
   cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n")
 }
 
+# ensure dependencies are installed ####
+if(!requireNamespace("aws.ec2metadata", quietly = TRUE)){
+  install.packages("aws.ec2metadata", dependencies = TRUE)
+}
+
 # install non-CRAN packages ####
 remotes::install_github("trafficonese/leaflet.extras", force = TRUE)
 remotes::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
-remotes::install_github("inbo/alien-species-portal@uat", 
+remotes::install_github("inbo/alien-species-portal@fix_v1.3.0", 
                          subdir = "alienSpecies", force = TRUE)
