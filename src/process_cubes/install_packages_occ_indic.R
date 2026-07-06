fe3ae0a49d21fa3e15e9f182358bc255a3376e6d
Sander Devisscher <sander.devisscher@inbo.be>
2026-07-06 16:18:50 +0200

    Update install_packages_occ_indic.R
    #640

--- src/process_cubes/install_packages_occ_indic.R
+++ src/process_cubes/install_packages_occ_indic.R
@@ -9,26 +9,3 @@ required <- c("remotes", "knitr", "dplyr", "magrittr", "rgbif", "tidylog",
 if (!all(required %in% installed)) {
   install.packages(required[!required %in% installed], dependencies = TRUE)
 }
-
-# Test if minimum version of aws.s3 is installed
-if (packageVersion("aws.s3") < "0.3.22") {
-  install.packages("devtools")
-  cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n
-      ==> installing v0.3.22 from https://rforge.net")
-  remotes::install_version("aws.s3", version = "0.3.22", repos = "https://rforge.net")
-}else{
-  cat("aws.s3 version:", paste(unlist(packageVersion("aws.s3")), collapse = "."), "\n")
-}
-
-# ensure dependencies are installed ####
-if(!requireNamespace("aws.ec2metadata", quietly = TRUE)){
-  warning("1st 'aws.ec2metadata' installation failed, retrying")
-  remotes::install_version("aws.ec2metadata", version = "0.2.0",
-                   dependencies = TRUE)
-}
-
-# install non-CRAN packages ####
-remotes::install_github("trafficonese/leaflet.extras", force = TRUE)
-remotes::install_github("inbo/INBOtheme@v0.5.9", force = TRUE)
-remotes::install_github("inbo/alien-species-portal@fix_v1.3.0", 
-                         subdir = "alienSpecies", force = TRUE)
